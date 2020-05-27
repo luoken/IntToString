6426a36e8bb909a19d24ff8670207830f83b55eb
Ken <luozhirong@gmail.com>
2020-05-27 16:57:56 -0700

    removes get_div_rem function

--- lib/integer_to_string.ex
+++ lib/integer_to_string.ex
@@ -7,39 +7,6 @@ defmodule IntegerToString do
       "eight thousand"
   """
 
-  @single_ints %{
-    0 => "zero",
-    1 => "one",
-    2 => "two",
-    3 => "three",
-    4 => "four",
-    5 => "five",
-    6 => "six",
-    7 => "seven",
-    8 => "eight",
-    9 => "nine"
-  }
-  @double_ints %{
-    10 => "ten",
-    11 => "eleven",
-    12 => "twelve",
-    13 => "thirteen",
-    14 => "fourteen",
-    15 => "fifteen",
-    16 => "sixteen",
-    17 => "seventeen",
-    18 => "eighteen",
-    19 => "nineteen",
-    20 => "twenty",
-    30 => "thirty",
-    40 => "fourty",
-    50 => "fifty",
-    60 => "sixty",
-    70 => "seventy",
-    80 => "eighty",
-    90 => "ninety"
-  }
-
   def convert(0), do: "zero"
   def convert(1), do: "one"
   def convert(2), do: "two"
@@ -70,31 +37,30 @@ defmodule IntegerToString do
   def convert(90), do: "ninety"
 
   def convert(integer) when rem(integer, 1000) == 0 do
-    {div, _val} = get_div_rem(integer, 1000)
+    div = div(integer, 1000)
     convert(div) <> " thousand"
   end
 
   def convert(integer) when integer > 1000 do
-    {div, val} = get_div_rem(integer, 1000)
+    div = div(integer, 1000)
+    val = rem(integer, 1000)
     convert(div) <> " thousand " <> convert(val)
   end
 
   def convert(integer) when integer >= 100 and integer <= 999 and rem(integer, 100) !== 0 do
-    {div, val} = get_div_rem(integer, 100)
+    div = div(integer, 100)
+    val = rem(integer, 100)
     convert(div) <> " hundred " <> convert(val)
   end
 
   def convert(integer) when rem(integer, 100) == 0 do
-    {div, _val} = get_div_rem(integer, 100)
+    div = div(integer, 100)
     convert(div) <> " hundred"
   end
 
   def convert(integer) when integer >= 10 and integer <= 99 do
-    {div, val} = get_div_rem(integer, 10)
+    div = div(integer, 10)
+    val = rem(integer, 10)
     convert(div * 10) <> "-" <> convert(val)
-  end
-
-  defp get_div_rem(integer, divisor) do
-    {div(integer, divisor), rem(integer, divisor)}
   end
 end
